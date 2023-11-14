97435f9d2be1d8e368ec687c81d2ca559bedb891
Abrafayansari <148965013+Abrafayansari@users.noreply.github.com>
2023-11-14 12:37:36 +0500

    Sign up form

--- App.jsx
+++ App.jsx
@@ -1,41 +1,37 @@
-import  Swal from "sweetalert2"
 import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Swal from 'sweetalert2'
+// import Navbar from './components/Navbar'
+import { Link } from 'react-router-dom'
 function App() {
- const [text,settext]=useState("")
- const [phone,setphone]=useState("")
- const [email,setemail]=useState("")
- const [name,setname]=useState("")
-  const handlesubmit=(e)=>{
+const handle = (e)=>{
   e.preventDefault();
   Swal.fire(
-    "Form Submitted",
-    `Name:${name} , phonenumber:${phone} , Email:${email}`
+    "you signed up"
   )
-
- }
+}
   return (
     <>
-    <div className="bg-orange-200 h-3/4 w-96"> 
-       <h1>Contact Form</h1>
-       <form onSubmit={handlesubmit}>
-<input  className = "border-2 m-2.5 w-80"     type="text"placeholder='Name' required  onChange={(e)=>{setname(e.target.value)}}/><br />
-<input className = "border-2 m-2.5 w-80"    type="text" placeholder='phone number'required onChange={(e)=>{setphone(e.target.value)}}/><br />
-<input  className = "border-2  m-2.5 w-80"   type="email"placeholder='Email' required onChange={(e)=>{setemail(e.target.value)}}/><br />
+    <div className="text-gray-600 flex ">
+      <form onSubmit={handle}>
+  <h1 className=" m-2 text-3xl font-bold mb-[60px] ml-28">Join Us</h1>
+<h4>Fullname</h4>
+<input  required className="border-2 m-2 border-black w-80 rounded-[12px]" type="text"  placeholder=  'Fullname' 
 
-<textarea className=" h-80 border-2  m-2.5 w-80" value={text}   type="text" onChange={(e)=>{
-  settext(e.target.value)
-}
-}  /> <br />
-<button  className="border-2 border-black " type="submit">
-  Submit
-</button>
-</form>
+/>
+<h4>Email</h4>
+<input required className="border-2 m-2 border-black w-80 rounded-[12px]" type="email" placeholder='email'/>
+<h4>password</h4>
+<input required className="border-2 m-2 border-black w-80 rounded-[12px]" type="password" placeholder='password' /><br />
+<button type='submit'
+className="ml-28 text-black w-24 p-2 bg-blue-600 w-28 mt-28 border-2 border-black rounded-[19px]">Sign up</button>
+      </form> 
     </div>
-    </>
+
+   </>
   )
 }
 
